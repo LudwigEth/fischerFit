2e82d338bd56a2aad9832f1bdac96ff94404ee2c
Ludwig.eth <ludwig.eth@gmail.com>
2024-04-02 21:17:33 +0200

    Add NavListItem component
    uses Following props:
        - liText
        Textcontent of the <a> element inside the <li>
        - liHref
        linkreference used for onsite scrollsnapping
        or triggering the contact form
        - delay
        optional transition delay value can be useful
        for dynamic delay lengths
        - menuOpened
        used to check if the menu has been opened
        to then add the delay thus starting it

--- src/components/header/NavListItem.jsx
+++ src/components/header/NavListItem.jsx
@@ -1,9 +1,12 @@
-const NavListItem = ({liText, liHref}) => {
-    return (
-        <li>
-            <a href={liHref}>{liText}</a>
-        </li>
-    )
+const NavListItem = ({ liText, liHref, delay, menuOpened }) => {
+  return (
+    <li
+      className={` transition-all duration-[250ms] ${menuOpened ? 'opacity-100' : 'opacity-0'}`}
+      style={menuOpened ? { transitionDelay: delay } : {}}
+    >
+      <a href={liHref}>{liText}</a>
+    </li>
+  )
 }
 
 export default NavListItem
